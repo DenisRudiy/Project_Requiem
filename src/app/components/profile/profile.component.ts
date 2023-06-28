8b96d5e197b77708a564562696176c3a4889aac8
DenisRudiy <denryd2311@gmail.com>
2023-06-28 17:06:31 +0300

    make template

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent {
-
+export class ProfileComponent implements OnInit{
+  constructor(){
+      
+  };
+  
+  ngOnInit(): void {
+    window.scrollTo(-1000, 0);
+  };
 }
